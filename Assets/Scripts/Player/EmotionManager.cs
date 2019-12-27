b4c9148fa9de72672d85aaf3d5cdde14112ba6a9
bluurrr <59037515+bluurrr@users.noreply.github.com>
2019-12-27 13:17:09 -0500

    Creating proper player prefabs

--- Assets/Scripts/Player/EmotionManager.cs
+++ Assets/Scripts/Player/EmotionManager.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class EmotionManager : MonoBehaviour
 {
-    public enum Emotions{idle, happy, sad, mad, randy};
+    public enum Emotions{idle, happy};
     public Emotions currentEmotion; 
 }
