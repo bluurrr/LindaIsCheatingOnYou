d1198cc59cc4715556105d62af1f319e5718564c
bluurrr <59037515+bluurrr@users.noreply.github.com>
2019-12-28 23:13:58 -0500

    Getting in functionality for assigning ik targets

---
+++ Assets/Scripts/Player/IKAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RootMotion.FinalIK;
+using UnityEngine;
+
+public class IKAnimation : MonoBehaviour
+{
+    private IKAnimationManager iKAnimationManager;
+    private InteractionTargetID[] interationcTargetID;
+    private List<InteractionTargetID> pinnedTargets = new List<InteractionTargetID>();
+
+    private void Awake()
+    {
+        Init();
+    }
+    private void Init()
+    {
+        iKAnimationManager = GetComponentInParent<IKAnimationManager>();
+        interationcTargetID = GetComponentsInChildren<InteractionTargetID>();
+        foreach(InteractionTargetID target in interationcTargetID)
+        {
+            if(target.animationType == InteractionTargetID.AnimationType.pin)
+            {
+                pinnedTargets.Add(target);
+            }
+        }  
+    }
+    public void Play()
+    {
+        Pin();
+    }
+
+    private void Pin()
+    {
+
+    }
+}
--- Assets/Scripts/Player/IKAnimationManager.cs
+++ Assets/Scripts/Player/IKAnimationManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Singletons;
+using RootMotion.FinalIK;
 
 public class IKAnimationManager : MonoBehaviour
 {
     public GameObject ModelRoot; 
+    public FullBodyBipedIK fullBodyBipedIK;
     private Dictionary<string, Transform> IKPoints =  new Dictionary<string, Transform>();
 
     public void Init()
@@ -23,6 +26,23 @@ public class IKAnimationManager : MonoBehaviour
             return IKPoints[key];
         }
         return null;
+    }
+
+    public void AssignIKTarget(InteractionTarget interactionTarget, Transform obj)
+    {
+        switch(interactionTarget.effectorType)
+        {
+            case RootMotion.FinalIK.FullBodyBipedEffector.RightHand:
+                fullBodyBipedIK.solver.rightHandEffector.target = obj;
+            break;
+            case RootMotion.FinalIK.FullBodyBipedEffector.LeftHand:
+                fullBodyBipedIK.solver.leftHandEffector.target = obj;
+            break;
+            case RootMotion.FinalIK.FullBodyBipedEffector.Body:
+                fullBodyBipedIK.solver.bodyEffector.target = obj;
+            break;  
+        }
+        fullBodyBipedIK.solver.leftHandEffector.target = obj; 
     }
 
 }
