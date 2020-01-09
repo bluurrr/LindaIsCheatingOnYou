7521c6bfba8547e09d57152660b7887089b7828c
bluurrr <59037515+bluurrr@users.noreply.github.com>
2020-01-08 21:58:54 -0500

    Adding hug offer

--- Assets/Scripts/Player/IKAnimation.cs
+++ Assets/Scripts/Player/IKAnimation.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class IKAnimation 
 {
     public string name;
-    public enum IK_Animation_ID {kiss, hug};
+    public enum IK_Animation_ID {kiss_offer, hug_hugging, hug_offer};
     public IK_Animation_ID iD; 
     public GameObject prefab;
     public Sprite icon;
